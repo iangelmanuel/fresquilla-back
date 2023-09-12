d8bd9b98daebd081b2275fd9232c0bdd92828be5
Angel Manuel <123855489+iAngelManuel@users.noreply.github.com>
2023-09-12 05:20:05 -0500

    🛠 FIX : Fixing settings that make errors

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,16 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 
 const corsOptions: CorsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || '',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }
 app.use(cors(corsOptions))
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY || "",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "",
+  api_key: process.env.CLOUDINARY_API_KEY || '',
+  api_secret: process.env.CLOUDINARY_API_SECRET || '',
 })
 
 app.use('/api/contact', ContactRouter)
